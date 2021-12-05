5534ee3f6d4e1c63587d6b0280d17200ecc347d4
Alexander <kulabuhov98@mail.ru>
2021-12-05 21:33:16 +0300

    Update Manager.cs

--- Apps/ToiletPaperLopushok/Classes/Manager.cs
+++ Apps/ToiletPaperLopushok/Classes/Manager.cs
@@ -4,7 +4,7 @@ namespace ToiletPaperLopushok.Classes
 {
     class Manager
     {
-        public static string TitlePage = "Информационная система произодственной компании \"Лопушок\""; /* Статичное свойство TitlePage для заголовка страницы. Задаем значение по умолчанию. */
+        public static string TitlePage = "Информационная система произодственной компании \"Лопушок\"."; /* Статичное свойство TitlePage для заголовка страницы. Задаем значение по умолчанию. */
         public static Frame MainFrame { get; set; } /* Статичное свойство MainFrame, для взаимодействия с Frame, размещенным в основном окне приложения. */
     }
 }
