71ba62965dbc5c4e6eaaae64c28e81dd519d81b3
Alexander <kulabuhov98@mail.ru>
2021-12-05 21:06:06 +0300

    Update MainWindow.xaml.cs
    Добавляем код для взаимодействия с ViewPage.xaml

--- Apps/ToiletPaperLopushok/Windows/MainWindow.xaml.cs
+++ Apps/ToiletPaperLopushok/Windows/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Windows;
 using ToiletPaperLopushok.Classes;
+using ToiletPaperLopushok.Pages;
 
 namespace ToiletPaperLopushok
 {
@@ -11,8 +12,12 @@ namespace ToiletPaperLopushok
         public MainWindow()
         {
             InitializeComponent();
-            TitlePageLabel.Content = Manager.TitlePage; /* Выводим основной заголовок страницы */
-            this.Title = "Информационная система производственной компании \"Лопушок\""; /* Задаем заголовок основного окна приложения */
+
+            TitlePageLabel.Content = Manager.TitlePage; /* Выводим основной заголовок страницы. */
+            this.Title = "Информационная система производственной компании \"Лопушок\""; /* Задаем заголовок основного окна приложения. */
+
+            Manager.MainFrame = MainFrame; /* Задаем статичному свойству значение MainFrame, для взаимодействия с Frame, размещенным в основном окне приложения. */
+            MainFrame.Navigate(new ViewPage()); /* Задаем основную страницу, загружаемую при запуске приложения. */
         }
     }
 }
