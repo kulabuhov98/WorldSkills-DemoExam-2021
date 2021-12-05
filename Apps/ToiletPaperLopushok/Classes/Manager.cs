c7d36099a1ca245d24a9b9dd47baf298f21f39e9
Alexander <kulabuhov98@mail.ru>
2021-12-05 19:22:17 +0300

    Create Manager.cs
    Задаем основной заголовок страницы

---
+++ Apps/ToiletPaperLopushok/Classes/Manager.cs
@@ -0,0 +1,7 @@
+﻿namespace ToiletPaperLopushok.Classes
+{
+    class Manager
+    {
+        public static string TitlePage = "Информационная система произодственной компании \"Лопушок\""; /* Задаем основной заголовок страницы */
+    }
+}
