23bc4bb34cf9fbc7f50772c5a3a8d00d2901446c
Alexander <kulabuhov98@mail.ru>
2021-12-05 21:04:20 +0300

    Create ViewPage.xaml

---
+++ Apps/ToiletPaperLopushok/Pages/ViewPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace ToiletPaperLopushok.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для ViewPage.xaml
+    /// </summary>
+    public partial class ViewPage : Page
+    {
+        public ViewPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
