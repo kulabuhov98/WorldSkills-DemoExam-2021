117b1897ce6bb1befb761fc68ae44d4b01310de7
Alexander <kulabuhov98@mail.ru>
2021-12-05 20:57:39 +0300

    Update Manager.cs
    Добавляем статичное свойство MainFrame.

--- Apps/ToiletPaperLopushok/Classes/Manager.cs
+++ Apps/ToiletPaperLopushok/Classes/Manager.cs
@@ -1,7 +1,10 @@
-﻿namespace ToiletPaperLopushok.Classes
+﻿using System.Windows.Controls;
+
+namespace ToiletPaperLopushok.Classes
 {
     class Manager
     {
-        public static string TitlePage = "Информационная система произодственной компании \"Лопушок\""; /* Задаем основной заголовок страницы */
+        public static string TitlePage = "Информационная система произодственной компании \"Лопушок\""; /* Статичное свойство TitlePage для заголовка страницы. Задаем значение по умолчанию. */
+        public static Frame MainFrame { get; set; } /* Статичное свойство MainFrame, для взаимодействия с Frame, размещенным в основном окне приложения. */
     }
 }
