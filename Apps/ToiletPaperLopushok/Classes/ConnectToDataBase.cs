9a9023fea34758f5addb9a34b4953e7783d4769e
Alexander <kulabuhov98@mail.ru>
2021-12-06 21:13:44 +0300

    Create ConnectToDataBase.cs

---
+++ Apps/ToiletPaperLopushok/Classes/ConnectToDataBase.cs
@@ -0,0 +1,9 @@
+﻿using ToiletPaperLopushok.Model;
+
+namespace ToiletPaperLopushok.Classes
+{
+    class ConnectToDataBase
+    {
+        public static ToiletPaperLopushokEntities ToiletPaperLopushok = new ToiletPaperLopushokEntities(); /* Статичное свойство ToiletPaperLopushok для взаимодействия с Базой Данных. */
+    }
+}
